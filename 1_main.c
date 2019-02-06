ed4748d5d0f22b9367d8d0c747ffc21c6d50fa52
Vlad Petrenko <HarewVlad@mail.ru>
2019-02-07 02:01:56 +0300

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -9,7 +9,7 @@
 #include <stdarg.h>
 #include <math.h>
 
-const char hex_to_base64[] = 
+const char hex_to_base64[] =
 {
 	'A',
 	'B',
@@ -79,7 +79,7 @@ const char hex_to_base64[] =
 	'/',
 };
 
-const char base64_to_hex[] = 
+const char base64_to_hex[] =
 {
 	['A'] = 0,
 	['B'] = 1,
@@ -150,25 +150,25 @@ const char base64_to_hex[] =
 };
 
 uint8_t char_to_digit[256] = {
-    ['0'] = 0,
-    ['1'] = 1,
-    ['2'] = 2,
-    ['3'] = 3,
-    ['4'] = 4,
-    ['5'] = 5,
-    ['6'] = 6,
-    ['7'] = 7,
-    ['8'] = 8,
-    ['9'] = 9,
-    ['a'] = 10, ['A'] = 10,
-    ['b'] = 11, ['B'] = 11,
-    ['c'] = 12, ['C'] = 12,
-    ['d'] = 13, ['D'] = 13,
-    ['e'] = 14, ['E'] = 14,
-    ['f'] = 15, ['F'] = 15,
+	['0'] = 0,
+	['1'] = 1,
+	['2'] = 2,
+	['3'] = 3,
+	['4'] = 4,
+	['5'] = 5,
+	['6'] = 6,
+	['7'] = 7,
+	['8'] = 8,
+	['9'] = 9,
+	['a'] = 10,['A'] = 10,
+	['b'] = 11,['B'] = 11,
+	['c'] = 12,['C'] = 12,
+	['d'] = 13,['D'] = 13,
+	['e'] = 14,['E'] = 14,
+	['f'] = 15,['F'] = 15,
 };
 
-char digit_to_char[256] = 
+char digit_to_char[256] =
 {
 	'0',
 	'1',
@@ -188,7 +188,7 @@ char digit_to_char[256] =
 	'f',
 };
 
-double letters_score[256] = 
+double letters_score[256] =
 {
 	['a'] = 8.167,
 	['b'] = 1.492,
@@ -243,18 +243,18 @@ double letters_score[256] =
 	['X'] = 0.150,
 	['Y'] = 1.974,
 	['Z'] = 0.074,
-	[' '] = 8.500,
+	[' '] = 7.500,
 };
 
 void fatal(const char *fmt, ...)
 {
-    va_list args;
-    va_start(args, fmt);
-    printf("FATAL: ");
-    vprintf(fmt, args);
-    printf("\n");
-    va_end(args);
-    exit(1);
+	va_list args;
+	va_start(args, fmt);
+	printf("FATAL: ");
+	vprintf(fmt, args);
+	printf("\n");
+	va_end(args);
+	exit(1);
 }
 
 void print_buff(const char *buff, size_t len, const char *fmt)
@@ -267,42 +267,42 @@ void print_buff(const char *buff, size_t len, const char *fmt)
 }
 
 int my_atoi(const char *source, const char *end, int base)
-{	
+{
 	int result = 0;
 	switch (base)
 	{
-		case 10:
-			while (source != end)
-			{
-				result *= 10;
-				result += *source++ - '0';
-			}
-			break;
-		case 2:
-			while (source != end)
-			{
-				result *= 2;
-				result += *source++ - '0';
-			}
-			break;
-		case 16:
-			while (source != end)
-			{
-				result *= 16;
-				result += char_to_digit[*source++];
-			}
-			break;
-		case 0xCC:
-			while (source != end)
-			{
-				result *= 256;
-				result += *source++ & 0xF0 | *source & 0x0F;
-			}
-			break;
-		default:
-			fatal("my_atoi: incorrect base");
+	case 10:
+		while (source != end)
+		{
+			result *= 10;
+			result += *source++ - '0';
+		}
+		break;
+	case 2:
+		while (source != end)
+		{
+			result *= 2;
+			result += *source++ - '0';
+		}
+		break;
+	case 16:
+		while (source != end)
+		{
+			result *= 16;
+			result += char_to_digit[*source++];
+		}
+		break;
+	case 0xCC:
+		while (source != end)
+		{
+			result *= 256;
+			result += *source++ & 0xF0 | *source & 0x0F;
+		}
+		break;
+	default:
+		fatal("my_atoi: incorrect base");
 	}
-	
+
 	return result;
 }
 
@@ -318,22 +318,22 @@ size_t bytes_in_file(FILE *f)
 	return result;
 }
 
-char *encode_ascii_to_base64(const char *string)
+char *encode_ascii_to_base64(const char *string, size_t len)
 {
 	const char *p_string = string;
-	const size_t len = strlen(string);
 	char *result_string = malloc(len * 4);
 	char *p_result_string = result_string;
 	int shift = 2;
 	int mask = 0x03;
 	int t = 0;
 	unsigned int value = 0;
-	while (*p_string != '\0')
+	while (len)
 	{
 		if (shift != 8)
 		{
 			value |= my_atoi(p_string, p_string + 1, 0xCC);
 			p_string++;
+			len--;
 		}
 		else
 			mask = 0x0;
@@ -358,14 +358,14 @@ char *encode_ascii_to_base64(const char *string)
 	return p_result_string;
 }
 
-char *decode_base64_to_ascii(const char *string, size_t *string_len)
+char *decode_base64_to_ascii(const char *string, size_t *string_len) // TODO: working not correctly at the end
 {
 	size_t len = strlen(string);
 	char *result = malloc(len);
 	char *p_result = result;
 	unsigned int value = 0;
 	char ch = 0;
-	for (int i = 0; i < len - 3; i += 4)
+	for (int i = 0; i < len; i += 4)
 	{
 		value = base64_to_hex[string[i]];
 		value = (value <<= 6) | base64_to_hex[string[i + 1]];
@@ -376,6 +376,7 @@ char *decode_base64_to_ascii(const char *string, size_t *string_len)
 		*result++ = value & 0xFF;
 		*string_len += 3;
 	}
+	*string_len -= 1;
 	*result = '\0';
 	return p_result;
 }
@@ -415,14 +416,14 @@ char *encode_hex_to_base64(const char *string)
 	}
 	t = value >> shift & 0x3F;
 	*result_string++ = hex_to_base64[t];
-	
+
 	*result_string = '\0';
 	return p_result_string;
 }
 
-char *fixed_xor(const char *str1, const char *str2, size_t len) // TODO: assert(strlen(str1) == strlen(str2))
+char *fixed_xor(const char *str1, const char *str2, size_t len)
 {
-	char *result = malloc(len);
+	char *result = malloc(len + 1);
 	for (int i = 0; i < len; i++)
 	{
 		result[i] = str1[i] ^ str2[i];
@@ -446,12 +447,12 @@ const char *decode_hex_encoded_string(const char *str, size_t len)
 	return p_result;
 }
 
-const char *break_single_byte_xor(const char *str, char *key, size_t len) // TODO: strange errors when double max, double count
+const char *break_single_byte_xor(const char *str, char *key, size_t len)
 {
-	char *temp = malloc(len);
-	int max = 0;
+	char *temp = malloc(len + 1);
+	double max = 0;
 	const char *final_result;
-	for (int i = 0; i < 256; i++)
+	for (int i = 0; i < 128; i++)
 	{
 		for (int j = 0; j < len; j++)
 		{
@@ -460,10 +461,11 @@ const char *break_single_byte_xor(const char *str, char *key, size_t len) // TOD
 		temp[len] = '\0';
 
 		const char *result = fixed_xor(str, temp, len);
-		int count = 0;
+		double count = 0;
 		for (int j = 0; j < len; j++)
 		{
-			count += letters_score[result[j]];
+			double temp = letters_score[(unsigned char)result[j]];
+			count += temp;
 		}
 
 		if (max < count)
@@ -474,7 +476,6 @@ const char *break_single_byte_xor(const char *str, char *key, size_t len) // TOD
 		}
 	}
 	free(temp);
-
 	return final_result;
 }
 
@@ -489,29 +490,31 @@ void read_string_from_file(char *dst, FILE *f)
 	*dst = '\0';
 }
 
-const char *break_single_byte_xor_from_file(FILE *f) // TODO: char str[256] should be char *str = malloc(...);
-{													// TODO: strange errors while double max, double count
+const char *break_single_byte_xor_from_file(FILE *f, char *key) // TODO: char str[256] should be char *str = malloc(...);
+{
 	assert(f != 0);
 	char str[256];
 	char ch = 0;
-	int max = 0;
+	double max = 0;
 	const char *final_result;
 	while (read_string_from_file(str, f), *str != '\0')
 	{
 		size_t len = strlen(str);
-		size_t decoded_len = len / 2;
 		const char *decoded_str = decode_hex_encoded_string(str, len);
+		size_t decoded_len = len / 2;
 		const char *result = break_single_byte_xor(decoded_str, &ch, decoded_len);
 
-		int count = 0;
-		for (int j = 0; j < len; j++)
+		double count = 0;
+		for (int j = 0; j < decoded_len; j++)
 		{
-			count += letters_score[result[j]];
+			double temp = letters_score[(unsigned char)result[j]];
+			count += temp;
 		}
 
 		if (max < count)
 		{
 			max = count;
+			*key = ch;
 			final_result = result;
 		}
 	}
@@ -522,7 +525,7 @@ const char *break_single_byte_xor_from_file(FILE *f) // TODO: char str[256] shou
 const char *repeated_key_xor(const char *str, const char *key, size_t len)
 {
 	size_t len_key = strlen(key);
-	char *result = malloc(len);
+	char *result = malloc(len + 1);
 	char *p_result = result;
 	for (int i = 0, j = 0; i < len; i++, j++, j %= len_key)
 	{
@@ -539,7 +542,7 @@ int hamming_distance(const char *str1, const char *str2, size_t len)
 	for (int i = 0; i < len; i++)
 	{
 		temp = str1[i] ^ str2[i];
-		for ( ; temp; temp >>= 1)
+		for (; temp; temp >>= 1)
 		{
 			result += temp & 1;
 		}
@@ -553,14 +556,15 @@ int find_keysize(const char *str, size_t len, int begin, int end)
 	double smallest_distance = 100500.0;
 	for (int i = begin; i < end; i++)
 	{
-		char *block_1 = malloc(i);
-		char *block_2 = malloc(i);
+		char *block_1 = malloc(i + 1);
+		char *block_2 = malloc(i + 1);
 
 		for (int j = 0; j < i; j++)
 		{
 			block_1[j] = str[j];
 			block_2[j] = str[j + i];
 		}
+
 		block_1[i] = '\0';
 		block_2[i] = '\0';
 
@@ -579,39 +583,39 @@ int find_keysize(const char *str, size_t len, int begin, int end)
 	return keysize;
 }
 
-const char *break_repeated_key_xor(const char *str, size_t len)
+const char *break_repeated_key_xor(const char *str, size_t len, int begin, int end)
 {
-	int keysize = find_keysize(str, len, 2, 40);
+	int keysize = find_keysize(str, len, begin, end);
 	size_t len_buff = len / keysize;
 	char *key = malloc(keysize);
 	for (int i = 0; i < keysize; i++)
 	{
-		char *buff = malloc(len_buff);
+		char *buff = malloc(len_buff + 1);
 
-		for (int j = 0, k = 0; j < len; j += keysize + i, k++)
+		for (int j = i, k = 0; j < len; j += keysize, k++)
 		{
-			buff[k] = str[j + keysize + i];
+			buff[k] = str[j + keysize];
 		}
+		buff[len_buff] = '\0';
 
 		const char *result = break_single_byte_xor(buff, &key[i], len_buff);
-
 		free(buff);
 	}
 	key[keysize] = '\0';
-	printf("Ch_6 - %s\n", key);
-	const char *result = repeated_key_xor(str, key, len);
-	return result;
+	printf("Key - %s\n", key);
+	return repeated_key_xor(str, key, len);
 }
 
 void ch_1()
 {
 	const char *test_1 = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.";
-	const char *result_1 = encode_ascii_to_base64(test_1);
+	size_t len = strlen(test_1);
+	const char *result_1 = encode_ascii_to_base64(test_1, len);
 	const char *answer_1 = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0"
-							"aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1"
-							"c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0"
-							"aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdl"
-							"LCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4";
+		"aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1"
+		"c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0"
+		"aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdl"
+		"LCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4";
 	assert(strcmp(result_1, answer_1) == 0);
 
 	const char *test_2 = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
@@ -639,7 +643,7 @@ void ch_3()
 	const char *test = decode_hex_encoded_string("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736", test_len);
 	char key = 0;
 	const char *result = break_single_byte_xor(test, &key, test_decoded_len);
-	printf("Ch_3 - %s\n", result);
+	printf("Ch_3 - Key - %c - %s\n", key, result);
 }
 
 void ch_4()
@@ -647,8 +651,9 @@ void ch_4()
 	FILE *f = fopen("4.txt", "r");
 	assert(f != 0);
 
-	const char *result = break_single_byte_xor_from_file(f);
-	printf("Ch_4 - %s\n", result);
+	char key = 0;
+	const char *result = break_single_byte_xor_from_file(f, &key);
+	printf("Ch_4 - Key - %c - %s\n", key, result);
 
 	fclose(f);
 }
@@ -661,6 +666,26 @@ void ch_5()
 	size_t test_2_len = strlen(test_2);
 	const char *result_1 = repeated_key_xor(test_1, "ICE", test_1_len);
 	const char *result_2 = repeated_key_xor(test_2, "ICE", test_2_len);
+}
+
+void ch_6()
+{
+	FILE *f = fopen("6.txt", "r");
+
+	if (!f)
+		fatal("main: fopen error");
+
+	size_t len = bytes_in_file(f);
+	char *str = malloc(len);
+	if (!fread(str, 1, len, f))
+		fatal("main: fread error");
+	str[len] = '\0';
+
+	size_t str_decoded_len = 0;
+	const char *str_decoded = decode_base64_to_ascii(str, &str_decoded_len);
+	const char *result = break_repeated_key_xor(str_decoded, str_decoded_len, 2, 20);
+
+	fclose(f);
 }
 
 int main(void)
@@ -681,23 +706,5 @@ int main(void)
 	ch_5();
 
 	// Ch_6
-	const char *test_1 = "this is a test";
-	const char *test_2 = "wokka wokka!!!";
-	size_t len_test_1 = strlen(test_1);
-	int result = hamming_distance(test_1, test_2, len_test_1);
-	int answer = 37;
-	assert(result == answer);
-
-	FILE *f = fopen("6.txt", "r");
-	assert(f != 0);
-
-	size_t len = bytes_in_file(f) - 1; // get rid of '='
-	char *str = malloc(len);
-	if (!fread(str, 1, len, f))
-		fatal("main: fread error");
-	str[len] = '\0';
-
-	size_t str_decoded_len = 0;
-	const char *str_decoded = decode_base64_to_ascii(str, &str_decoded_len);
-	const char *test = break_repeated_key_xor(str_decoded, str_decoded_len);
+	ch_6();
 }
